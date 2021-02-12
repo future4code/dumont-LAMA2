af0f898cb462b7c521e17b6a4210d8be5aa2b383
joyceroberta <joycinha0406@gmail.com>
2021-02-12 17:03:03 -0300

    Correção do endpoint de criação de shows, validações OK

--- lama-template/src/controller/ShowController.ts
+++ lama-template/src/controller/ShowController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { BandBusiness } from "../business/BandBusiness";
 import { Show, ShowInputDTO } from "../business/entities/Show";
 import { Authenticator } from "../business/services/Authenticator";
 import { IdGenerator } from "../business/services/IdGenerator";
@@ -7,24 +6,29 @@ import { ShowBusiness } from "../business/ShowBusiness";
 import { BandDatabase } from "../data/BandDatabase";
 import { ShowDatabase } from "../data/ShowDatabase";
 
-
 export class ShowController {
-  constructor(
-    private showBusiness: ShowBusiness) {}
 
-  public createShow = async (req: Request, res: Response): Promise<void> => {
+
+  async createShow (req: Request, res: Response){
     try {
       const week_day = Show.toWeekDayEnum(req.body.week_day);
-      const input: ShowInputDTO ={
+      const input: ShowInputDTO = {
         band_id: req.body.band_id,
         week_day,
         start_time: req.body.start_time,
         end_time: req.body.end_time
       }
 
-      const token = req.headers.authorization as string;
+      const showBusiness = new ShowBusiness(
+        new ShowDatabase,
+        new BandDatabase,
+        new IdGenerator,
+        new Authenticator
+      )
 
-      await this.showBusiness.createShow(input, token)
+      await showBusiness.createShow(input, req.headers.authorization as string)
+
+    
 
       res.status(200).send({message: "Show created!"})
 
--- lama-template/src/data/ShowDatabase.ts
+++ lama-template/src/data/ShowDatabase.ts
@@ -1,4 +1,4 @@
-import { Show, ShowOutputDTO, WeekDay } from "../business/entities/Show";
+import { Show, WeekDay } from "../business/entities/Show";
 import { CustomError } from "../business/error/CustomError";
 import { BaseDatabase } from "../data/BaseDatabase";
 
@@ -21,35 +21,25 @@ export class ShowDatabase extends BaseDatabase {
     }
   }
 
-//   public async getShowsByTimes(
-//     week_day: WeekDay,
-//     start_time: number,
-//     end_time: number
-//   ): Promise<ShowOutputDTO> {
-//     const shows = await BaseDatabase.connection.raw
-//     {
-//         `
-//         SELECT show.id as id,
-//                 show.start_time as start_time,
-//                 show.end_time as end_time,
-//                 show.week_day as week_day
-//         FROM ${ShowDatabase.TABLE_NAME} show 
-//         WHERE show.week_day = "${week_day}"
-//         AND WHERE show.start_time <= "${end_time}"
-//         AND WHERE show.end_time >= "${start_time}"
-//         ORDER BY start_time ASC
-//         `
-//     }
+  public async getShowsByTimes(
+    week_day: WeekDay,
+    start_time: number,
+    end_time: number
+  ): Promise<any> {
+    const shows = await BaseDatabase.connection
+    .select ("*")
+    .where("end_time", ">", `${start_time}`)
+    .andWhere("start_time", "<", `${end_time}`)
+    .from(ShowDatabase.TABLE_NAME)
 
-//     return shows.map((show: any)=>{
-//         return {
-//             id: show.id,
-//             band_id: show.band_id,
-//             start_time: show.start_time,
-//             end_time: show.end_time,
-//             week_day: show.week_day
-
-//         }
-//     })
-//   }
+    return shows.map((show: any)=>{
+        return {
+            id: show.id,
+            band_id: show.band_id,
+            start_time: show.start_time,
+            end_time: show.end_time,
+            week_day: show.week_day
+        }
+    })
+  }
 }
